6e09eb54f5a58e4d411f6188b3fd47161e57337e
yt8492 <tmymyut2000@gmail.com>
2021-10-23 14:41:39 +0900

    CommonModuleの実装

--- app/src/main/java/com/dawn/android/DawnApplication.kt
+++ app/src/main/java/com/dawn/android/DawnApplication.kt
@@ -1,17 +1,20 @@
 package com.dawn.android
 
 import android.app.Application
+import com.dawn.android.common.commonModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
 
 class DawnApplication : Application() {
-
     override fun onCreate() {
         super.onCreate()
         startKoin {
             androidLogger()
             androidContext(this@DawnApplication)
+            modules(
+                commonModule,
+            )
         }
     }
 }
