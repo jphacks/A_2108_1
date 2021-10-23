b72651cc693722c7a3968056fa5f8253dcbc339e
yt8492 <tmymyut2000@gmail.com>
2021-10-23 17:10:30 +0900

    Appコンポーネントの実装

--- app/src/main/java/com/dawn/android/ui/MainActivity.kt
+++ app/src/main/java/com/dawn/android/ui/MainActivity.kt
@@ -3,11 +3,6 @@ package com.dawn.android.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 import com.dawn.android.ui.theme.DawnTheme
 
 class MainActivity : ComponentActivity() {
@@ -15,24 +10,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             DawnTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+                App()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    DawnTheme {
-        Greeting("Android")
     }
 }
