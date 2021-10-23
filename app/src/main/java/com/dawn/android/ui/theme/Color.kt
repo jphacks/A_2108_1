1b74539e71239bee9cbed1ffc1b58a987bf9cafc
yt8492 <tmymyut2000@gmail.com>
2021-10-23 11:59:33 +0900

    不要なColorの削除

--- app/src/main/java/com/dawn/android/ui/theme/Color.kt
+++ app/src/main/java/com/dawn/android/ui/theme/Color.kt
@@ -2,11 +2,6 @@ package com.dawn.android.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
-
 val MainColor = Color(0xFFFDD835)
 val SubMainColor = Color(0xFFFEF3C2)
 val AccentBlue = Color(0xFF015BB3)
