83cac074823b9ba1a97708fac8b854233513ff5b
yt8492 <tmymyut2000@gmail.com>
2021-10-23 11:59:08 +0900

    Themeの実装

--- app/src/main/java/com/dawn/android/ui/theme/Theme.kt
+++ app/src/main/java/com/dawn/android/ui/theme/Theme.kt
@@ -1,42 +1,21 @@
 package com.dawn.android.ui.theme
 
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
-private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
-)
-
-private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
+private val ColorPalette = lightColors(
+    primary = Gray900,
+    primaryVariant = Gray700,
+    secondary = MainColor,
+    secondaryVariant = SubMainColor,
+    error = AccentRed,
 )
 
 @Composable
-fun DawnTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
-
+fun DawnTheme(content: @Composable() () -> Unit) {
     MaterialTheme(
-        colors = colors,
+        colors = ColorPalette,
         typography = Typography,
         shapes = Shapes,
         content = content
