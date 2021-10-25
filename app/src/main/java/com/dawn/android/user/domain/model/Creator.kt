ff0dceb0084399da3e74e80e777d13f4c39aaab5
yt8492 <tmymyut2000@gmail.com>
2021-10-25 23:40:38 +0900

    userのドメイン層のモデルの実装

---
+++ app/src/main/java/com/dawn/android/user/domain/model/Creator.kt
@@ -0,0 +1,7 @@
+package com.dawn.android.user.domain.model
+
+data class Creator(
+    val id: CreatorId,
+    val imageUrl: String,
+    val displayName: String,
+)
---
+++ app/src/main/java/com/dawn/android/user/domain/model/JobId.kt
@@ -0,0 +1,3 @@
+package com.dawn.android.user.domain.model
+
+data class JobId(val value: Long)
