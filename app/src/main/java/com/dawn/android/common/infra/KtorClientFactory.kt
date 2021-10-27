b12dd977e91aae16fb0fe6209b260556f6c854bd
yt8492 <tmymyut2000@gmail.com>
2021-10-27 18:57:32 +0900

    KtorClientがKotlinxSerializerを使うようにする

--- app/src/main/java/com/dawn/android/common/infra/KtorClientFactory.kt
+++ app/src/main/java/com/dawn/android/common/infra/KtorClientFactory.kt
@@ -3,11 +3,16 @@ package com.dawn.android.common.infra
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.cio.CIO
 import io.ktor.client.features.defaultRequest
+import io.ktor.client.features.json.JsonFeature
+import io.ktor.client.features.json.serializer.KotlinxSerializer
 import io.ktor.http.URLProtocol
 
 class KtorClientFactory {
     fun provide(): HttpClient {
         return HttpClient(CIO) {
+            install(JsonFeature) {
+                serializer = KotlinxSerializer()
+            }
             defaultRequest {
                 url.host = "dawn.shinbunbun.info"
                 url.protocol = URLProtocol.HTTPS
