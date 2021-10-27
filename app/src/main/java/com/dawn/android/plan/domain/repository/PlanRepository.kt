63a4427f3344678bd87151f36006ee4545b56f4f
yt8492 <tmymyut2000@gmail.com>
2021-10-27 18:57:14 +0900

    Repositoryのメソッドをsuspend functionにする

--- app/src/main/java/com/dawn/android/plan/domain/repository/PlanRepository.kt
+++ app/src/main/java/com/dawn/android/plan/domain/repository/PlanRepository.kt
@@ -5,6 +5,6 @@ import com.dawn.android.plan.domain.model.PlanDetail
 import com.dawn.android.plan.domain.model.PlanId
 
 interface PlanRepository {
-    fun getAll(): List<Plan>
-    fun get(id: PlanId): PlanDetail
+    suspend fun getAll(): List<Plan>
+    suspend fun get(id: PlanId): PlanDetail
 }
