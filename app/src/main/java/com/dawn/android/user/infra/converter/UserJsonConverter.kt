ce0ab48f657b48282d4f972a6a7bea108943ca6f
yt8492 <tmymyut2000@gmail.com>
2021-10-29 06:05:53 +0900

    UserRegistrationをJsonに変換するconverterの実装

--- app/src/main/java/com/dawn/android/user/infra/converter/UserJsonConverter.kt
+++ app/src/main/java/com/dawn/android/user/infra/converter/UserJsonConverter.kt
@@ -1,5 +1,6 @@
 package com.dawn.android.user.infra.converter
 
+import com.dawn.android.common.ui.toRFC3339
 import com.dawn.android.plan.domain.model.PlaceId
 import com.dawn.android.user.domain.model.Contact
 import com.dawn.android.user.domain.model.Creator
@@ -7,10 +8,15 @@ import com.dawn.android.user.domain.model.CreatorId
 import com.dawn.android.user.domain.model.Job
 import com.dawn.android.user.domain.model.JobId
 import com.dawn.android.user.domain.model.Place
+import com.dawn.android.user.domain.model.Sex
+import com.dawn.android.user.domain.model.Token
+import com.dawn.android.user.domain.model.UserRegistration
 import com.dawn.android.user.infra.api.json.ContactJson
 import com.dawn.android.user.infra.api.json.JobJson
 import com.dawn.android.user.infra.api.json.CreatorUserJson
 import com.dawn.android.user.infra.api.json.PlaceJson
+import com.dawn.android.user.infra.api.json.TokenJson
+import com.dawn.android.user.infra.api.json.UserRegistrationJson
 import java.time.Instant
 
 object UserJsonConverter {
@@ -49,6 +55,50 @@ object UserJsonConverter {
             prefecture = json.area,
             city = json.city,
             name = json.name,
+        )
+    }
+
+    fun convertToDomainModel(json: TokenJson): Token {
+        return Token(json.token)
+    }
+
+    fun convertToJson(model: Contact): ContactJson {
+        return ContactJson(
+            id = 0,
+            hpLink = model.hpLink,
+            instagramLink = model.instagramLink,
+            twitterLink = model.twitterLink,
+            facebookLink = model.facebookLink,
+            tiktokLink = model.tiktokLink,
+            biography = model.biography,
+        )
+    }
+
+    fun convertToJson(model: Place): PlaceJson {
+        return PlaceJson(
+            id = model.id.value,
+            area = model.area,
+            prefecture = model.prefecture,
+            city = model.city,
+            name = model.name,
+        )
+    }
+
+    fun convertToJson(model: UserRegistration): UserRegistrationJson {
+        return UserRegistrationJson(
+            userName = model.userName,
+            email = model.email,
+            password = model.password,
+            image = model.imageUrl,
+            displayName = model.displayName,
+            dateOfBirth = model.dateOfBirth.toRFC3339(),
+            sex = when (model.sex) {
+                Sex.Male -> 0
+                Sex.Female -> 1
+                Sex.Other -> 2
+            },
+            contacts = convertToJson(model.contact),
+            address = convertToJson(model.place),
         )
     }
 }
