12dc89b206512c093a6197292d4e139c23669791
yt8492 <tmymyut2000@gmail.com>
2021-10-29 01:39:12 +0900

    MaskedUserJsonの存在しないプロパティを削除

--- app/src/main/java/com/dawn/android/user/infra/api/json/MaskedUserJson.kt
+++ app/src/main/java/com/dawn/android/user/infra/api/json/MaskedUserJson.kt
@@ -7,12 +7,9 @@ import kotlinx.serialization.Serializable
 data class MaskedUserJson(
     val id: Long,
     val userName: String,
-    val email: String,
     @SerialName("image")
     val imageUrl: String,
     val displayName: String,
-    val dateOfBirth: String,
-    val sex: Int,
     val contacts: ContactJson,
     val creator: CreatorJson,
 )
