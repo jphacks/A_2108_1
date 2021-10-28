d9d54582724b5c9cc476a2bf0cad78579c16be19
yt8492 <tmymyut2000@gmail.com>
2021-10-28 21:47:02 +0900

    JsonSerializerが未定義のkeyを無視するようにする

--- app/src/main/java/com/dawn/android/common/infra/KtorClientFactory.kt
+++ app/src/main/java/com/dawn/android/common/infra/KtorClientFactory.kt
@@ -6,12 +6,17 @@ import io.ktor.client.features.defaultRequest
 import io.ktor.client.features.json.JsonFeature
 import io.ktor.client.features.json.serializer.KotlinxSerializer
 import io.ktor.http.URLProtocol
+import kotlinx.serialization.json.Json
 
 class KtorClientFactory {
     fun provide(): HttpClient {
         return HttpClient(CIO) {
             install(JsonFeature) {
-                serializer = KotlinxSerializer()
+                serializer = KotlinxSerializer(
+                    Json {
+                        ignoreUnknownKeys = true
+                    }
+                )
             }
             defaultRequest {
                 url.host = "dawn.shinbunbun.info"
