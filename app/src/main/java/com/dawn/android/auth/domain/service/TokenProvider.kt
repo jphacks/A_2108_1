5b22262dbf7f1ad5fb979a000aa8dff19a87995a
yt8492 <tmymyut2000@gmail.com>
2021-10-29 07:54:44 +0900

    TokenProviderとSignUpServiceの削除

--- app/src/main/java/com/dawn/android/auth/domain/service/TokenProvider.kt
+++
@@ -1,9 +0,0 @@
-package com.dawn.android.auth.domain.service
-
-import com.dawn.android.auth.domain.model.Token
-
-interface TokenProvider {
-    suspend fun provide(): Token?
-    suspend fun save(token: Token)
-    suspend fun delete()
-}
