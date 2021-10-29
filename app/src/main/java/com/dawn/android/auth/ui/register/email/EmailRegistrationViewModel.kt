02b1c4ccb6edee2a636f7a4a1d8d5ba9af8e529f
yt8492 <tmymyut2000@gmail.com>
2021-10-30 00:22:49 +0900

    EmailRegistrationViewModelの実装

--- app/src/main/java/com/dawn/android/auth/ui/register/email/EmailRegistrationViewModel.kt
+++ app/src/main/java/com/dawn/android/auth/ui/register/email/EmailRegistrationViewModel.kt
@@ -2,8 +2,12 @@ package com.dawn.android.auth.ui.register.email
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.dawn.android.auth.domain.model.RegistrationResult
+import com.dawn.android.auth.domain.model.UserRegistration
 import com.dawn.android.auth.domain.service.AccountService
 import com.dawn.android.user.domain.model.Area
+import com.dawn.android.user.domain.model.City
+import com.dawn.android.user.domain.model.Contact
 import com.dawn.android.user.domain.model.Prefecture
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -15,7 +19,25 @@ class EmailRegistrationViewModel(
     private val _currentState = MutableStateFlow<EmailRegistrationState>(EmailRegistrationState.Init)
     val currentState: StateFlow<EmailRegistrationState> get() = _currentState
 
+    private val _loading = MutableStateFlow(false)
+    val loading get() = _loading
 
+    private val _finish = MutableStateFlow(false)
+    val finish get() = _finish
+
+    fun accountInfo(
+        email: String,
+        password: String,
+        nickname: String,
+        userId: String,
+    ) {
+        _currentState.value = EmailRegistrationState.NicknameUserId(
+            email,
+            password,
+            nickname,
+            userId,
+        )
+    }
 
 //    fun emailPassword(
 //        email: String,
@@ -121,70 +143,172 @@ class EmailRegistrationViewModel(
 //        }
 //    }
 //
-//    fun area(area: Area) {
-//        viewModelScope.launch {
-//            when (val state = currentState.value) {
-//                is EmailRegistrationState.NicknameUserId -> {
-//                    _currentState.value = state.next(area)
-//                }
-//                is EmailRegistrationState.AddressArea -> {
-//                    _currentState.value = state.copy(
-//                        area = area,
-//                    )
-//                }
-//                is EmailRegistrationState.AddressPrefecture -> {
-//                    _currentState.value = state.copy(
-//                        area = area,
-//                    )
-//                }
-//                is EmailRegistrationState.AddressPlace -> {
-//                    _currentState.value = state.copy(
-//                        area = area,
-//                        place = state.place.copy(
-//                            area = area.id,
-//                        )
-//                    )
-//                }
-//                is EmailRegistrationState.Biography -> {
-//                    _currentState.value = state.copy(
-//                        area = area,
-//                        place = state.place.copy(
-//                            area = area.id,
-//                        )
-//                    )
-//                }
-//                is EmailRegistrationState.Contacts -> {
-//                    _currentState.value = state.copy(
-//                        area = area,
-//                    )
-//                }
-//                else -> {
-//                    // invalid
-//                }
-//            }
-//        }
-//    }
-//
-//    fun prefecture(prefecture: Prefecture) {
-//        viewModelScope.launch {
-//            when (val state = currentState.value) {
-//                is EmailRegistrationState.AddressArea -> {
-//                    _currentState.value = state.next(prefecture)
-//                }
-//                is EmailRegistrationState.AddressPrefecture -> {
-//                    _currentState.value = state.copy(
-//                        prefecture = prefecture,
-//                    )
-//                }
-//                is EmailRegistrationState.AddressPlace -> {
-//                    _currentState.value = state.copy(
-//                        prefecture = prefecture,
-//                        place = state.place.copy(
-//                            prefecture = prefecture.id
-//                        )
-//                    )
-//                }
-//            }
-//        }
-//    }
+    fun area(area: Area) {
+        viewModelScope.launch {
+            when (val state = currentState.value) {
+                is EmailRegistrationState.NicknameUserId -> {
+                    _currentState.value = state.next(area)
+                }
+                is EmailRegistrationState.AddressArea -> {
+                    _currentState.value = state.copy(
+                        area = area,
+                    )
+                }
+                is EmailRegistrationState.AddressPrefecture -> {
+                    _currentState.value = state.copy(
+                        area = area,
+                    )
+                }
+                is EmailRegistrationState.AddressPlace -> {
+                    _currentState.value = state.copy(
+                        area = area,
+                        place = state.place.copy(
+                            area = area.id,
+                        )
+                    )
+                }
+                is EmailRegistrationState.Biography -> {
+                    _currentState.value = state.copy(
+                        area = area,
+                        place = state.place.copy(
+                            area = area.id,
+                        )
+                    )
+                }
+                is EmailRegistrationState.Contacts -> {
+                    _currentState.value = state.copy(
+                        area = area,
+                    )
+                }
+                else -> {
+                    // invalid
+                }
+            }
+        }
+    }
+
+    fun prefecture(prefecture: Prefecture) {
+        viewModelScope.launch {
+            when (val state = currentState.value) {
+                is EmailRegistrationState.AddressArea -> {
+                    _currentState.value = state.next(prefecture)
+                }
+                is EmailRegistrationState.AddressPrefecture -> {
+                    _currentState.value = state.copy(
+                        prefecture = prefecture,
+                    )
+                }
+                is EmailRegistrationState.AddressPlace -> {
+                    _currentState.value = state.copy(
+                        prefecture = prefecture,
+                        place = state.place.copy(
+                            prefecture = prefecture.id
+                        )
+                    )
+                }
+                is EmailRegistrationState.Biography -> {
+                    _currentState.value = state.copy(
+                        prefecture = prefecture,
+                        place = state.place.copy(
+                            prefecture = prefecture.id
+                        )
+                    )
+                }
+                is EmailRegistrationState.Contacts -> {
+                    _currentState.value = state.copy(
+                        prefecture = prefecture,
+                        place = state.place.copy(
+                            prefecture = prefecture.id
+                        )
+                    )
+                }
+            }
+        }
+    }
+
+    fun city(city: City) {
+        viewModelScope.launch {
+            when (val state = currentState.value) {
+                is EmailRegistrationState.AddressPrefecture -> {
+                    _currentState.value = state.next(city)
+                }
+                is EmailRegistrationState.AddressPlace -> {
+                    _currentState.value = state.copy(
+                        place = state.place.copy(
+                            city = city.id,
+                            name = state.area.name + state.prefecture.name + city.name,
+                        )
+                    )
+                }
+                is EmailRegistrationState.Biography -> {
+                    _currentState.value = state.copy(
+                        place = state.place.copy(
+                            city = city.id,
+                            name = state.area.name + state.prefecture.name + city.name,
+                        )
+                    )
+                }
+                is EmailRegistrationState.Contacts -> {
+                    _currentState.value = state.copy(
+                        place = state.place.copy(
+                            city = city.id,
+                            name = state.area.name + state.prefecture.name + city.name,
+                        )
+                    )
+                }
+            }
+        }
+    }
+
+    fun biography(biography: String) {
+        viewModelScope.launch {
+            when (val state = currentState.value) {
+                is EmailRegistrationState.AddressPlace -> {
+                    _currentState.value = state.next(biography)
+                }
+                is EmailRegistrationState.Biography -> {
+                    _currentState.value = state.copy(
+                        biography = biography,
+                    )
+                }
+                is EmailRegistrationState.Contacts -> {
+                    _currentState.value = state.copy(
+                        biography = biography,
+                    )
+                }
+            }
+        }
+    }
+
+    fun contact(contact: Contact) {
+        viewModelScope.launch {
+            when (val state = currentState.value) {
+                is EmailRegistrationState.Biography -> {
+                    val request = state.next(contact).toUserRegistration()
+                    register(request)
+                }
+                is EmailRegistrationState.Contacts -> {
+                    val next = state.copy(
+                        contact = contact,
+                    )
+                    val request = next.toUserRegistration()
+                    register(request)
+                }
+            }
+        }
+    }
+
+    private suspend fun register(userRegistration: UserRegistration) {
+        _loading.value = true
+        when (accountService.register(userRegistration)) {
+            is RegistrationResult.Success -> {
+                _loading.value = false
+                _finish.value = true
+            }
+            is RegistrationResult.Failure -> {
+                _loading.value = false
+                _finish.value = true
+            }
+        }
+    }
 }
