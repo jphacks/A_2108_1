1157e62f8a1191d5c088def6f5114852e965bc02
yt8492 <tmymyut2000@gmail.com>
2021-10-29 19:53:45 +0900

    地域のmodelの追加

---
+++ app/src/main/java/com/dawn/android/user/domain/model/City.kt
@@ -0,0 +1,6 @@
+package com.dawn.android.user.domain.model
+
+data class City(
+    val id: Int,
+    val name: String,
+)
---
+++ app/src/main/java/com/dawn/android/user/domain/model/Prefecture.kt
@@ -0,0 +1,6 @@
+package com.dawn.android.user.domain.model
+
+data class Prefecture(
+    val id: Int,
+    val name: String,
+)
