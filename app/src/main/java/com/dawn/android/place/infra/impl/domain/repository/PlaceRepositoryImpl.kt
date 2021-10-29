3a9688cac2f5e373bef179a1d1b2f48a9c701227
yt8492 <tmymyut2000@gmail.com>
2021-10-30 01:11:15 +0900

    PlaceRepositoryImplの実装

---
+++ app/src/main/java/com/dawn/android/place/infra/impl/domain/repository/PlaceRepositoryImpl.kt
@@ -0,0 +1,44 @@
+package com.dawn.android.place.infra.impl.domain.repository
+
+import com.dawn.android.place.domain.model.Area
+import com.dawn.android.place.domain.model.City
+import com.dawn.android.place.domain.model.Prefecture
+import com.dawn.android.place.domain.repository.PlaceRepository
+import com.dawn.android.place.infra.api.PlaceApi
+
+class PlaceRepositoryImpl(
+    private val placeApi: PlaceApi,
+) : PlaceRepository {
+    override suspend fun getAreas(): List<Area> {
+        return placeApi
+            .getAreas()
+            .map {
+                Area(
+                    id = it.area,
+                    name = it.name,
+                )
+            }
+    }
+
+    override suspend fun getPrefectures(areaId: Int): List<Prefecture> {
+        return placeApi
+            .getPrefectures(areaId)
+            .map {
+                Prefecture(
+                    id = it.prefecture,
+                    name = it.name,
+                )
+            }
+    }
+
+    override suspend fun getCities(prefectureId: Int): List<City> {
+        return placeApi
+            .getCities(prefectureId)
+            .map {
+                City(
+                    id = it.city,
+                    name = it.name,
+                )
+            }
+    }
+}
