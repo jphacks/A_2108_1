f1bb9f84f85a178bb5cafd659437420776d3f239
yt8492 <tmymyut2000@gmail.com>
2021-10-30 00:27:42 +0900

    ユーザー登録のNavItemsの追加

--- app/src/main/java/com/dawn/android/auth/ui/AuthNavItems.kt
+++ app/src/main/java/com/dawn/android/auth/ui/AuthNavItems.kt
@@ -2,4 +2,10 @@ package com.dawn.android.auth.ui
 
 sealed class AuthNavItems(val route: String) {
     object RegisterEntry : AuthNavItems("RegisterEntry")
+    object AccountInfo : AuthNavItems("AccountInfo")
+    object Area : AuthNavItems("Area")
+    object Prefecture : AuthNavItems("Prefecture")
+    object City : AuthNavItems("City")
+    object Biography : AuthNavItems("Biography")
+    object Contact : AuthNavItems("Contact")
 }
